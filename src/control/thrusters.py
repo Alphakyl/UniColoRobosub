d2c2ebe107a8ff9e2f1376d064fa936bce1f4a8e
Christopher Goes <cwg46@cornell.edu>
2015-07-24 18:41:34 -0400

    Working thruster code.

--- src/control/thrusters.py
+++ src/control/thrusters.py
@@ -1,11 +1,19 @@
 import serial, time
 
-chr = lambda x: bytes([x])
+chr = lambda x: bytearray([x])
 
 ser = serial.Serial('/dev/ttyACM0')
+# ser.baudrate = 9600
 ser.write(chr(0xAA))
 ser.flush()
 
+MIN, MAX = 21, 234
+
+inR = lambda x: max(0, min(256, x))
+
+# Remap -1 to 1.
+
 def mset(channel, target):
-  raw = chr(0x84) + chr(channel) + chr(target & 0x7F) + chr((target >> 7) & 0x7F)
+  raw = chr(0xFF) + chr(channel) + chr(inR(MIN + int(round((MAX - MIN) * (1 + target) / 2))))
   ser.write(raw)
+  ser.flush()
