34df7c8faa8a0cf001da53258838bfe4b69dcb70
Bobblehead0 <dybo6303@colorado.edu>
2015-07-24 02:34:14 -0700

    sparton code done and tested, starting pressure sensor and dvl code

--- src/sensors/dvl.py
+++ src/sensors/dvl.py
@@ -4,10 +4,34 @@ DVL Interface
 
 '''
 
+from threading import Lock,Thread
+import serial
+
 class DVL:
   def __init__(self):
-    pass
+    virtualFile = open("/var/run/dvl")
+    self.lock = Lock()
+    self.thread = Thread(self.loopMain)
+    self.thread.start()
+  def loopMain(self):
+      for line in self.virtualFile:
+          splitline = line.split(",")
+          if splitline[0] == "DVL":
+              try:
+                  self.lock.acquire()
+                  self.xVelocity = float(splitline[1])
+                  self.yVelocity = float(splitline[2])
+                  self.zVelocity = float(splitline[3])
+              except ValueError,IndexError:
+                  pass
+              finally:
+                  self.lock.release()
+
+
 
   # Return velx, vely
   def read(self):
-    return [0., 0.]
+      self.lock.acquire()
+      results = (self.xVelocity,self.yVelocity,self.zVelocity)
+      self.lock.release()
+      return results
